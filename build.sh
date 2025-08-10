05fe22c75283ed41ba6ff506c8e0b13f469f673b
Matthew Laux <matthew.laux@gmail.com>
2025-08-10 16:51:42 -0500

    add build date to output

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sed "s/\${GIT_SHA}/$(git rev-parse --short HEAD)/g" crane.html > index.html
+sed -e "s/\${GIT_SHA}/$(git rev-parse --short HEAD)/g" \
+    -e "s/\${BUILD_DATE}/$(date)/g" \
+    crane.html > index.html
